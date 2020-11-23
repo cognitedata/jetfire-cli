1c902eb77147d694c154fafe42ccdc437a1cbbf9
Mathias Lohne <mathias.lohne@cognite.com>
2020-11-23 13:03:56 +0100

    Add support for using OIDC tokens to auth against jetfire api

--- Cognite.Jetfire.Api/Credentials.cs
+++ Cognite.Jetfire.Api/Credentials.cs
@@ -4,13 +4,16 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
+using Cognite.Extractor.Utils;
 using Cognite.Jetfire.Api.Model;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Cognite.Jetfire.Api
 {
     public interface ICredentials
     {
-        void Apply(HttpRequestMessage request);
+        Task ApplyAsync(HttpRequestMessage request);
     }
 
     public sealed class ApiKeyCredentials : ICredentials
@@ -22,9 +25,30 @@ namespace Cognite.Jetfire.Api
             ApiKey = apiKey;
         }
 
-        public void Apply(HttpRequestMessage request)
+        public async Task ApplyAsync(HttpRequestMessage request)
         {
             request.Headers.Add("api-key", ApiKey);
+        }
+    }
+
+    public sealed class TokenCredentials : ICredentials
+    {
+        public string ClientId { get; set; }
+        public string Secret { get; set; }
+        public List<string> Scopes { get; set; }
+        public string Authority { get; set; }
+
+        private IAuthenticator Authenticator;
+
+        public TokenCredentials(AuthenticatorConfig authenticatorConfig)
+        {
+            Authenticator = new Authenticator(authenticatorConfig, new HttpClient(), NullLogger<IAuthenticator>.Instance);
+        }
+
+        public async Task ApplyAsync(HttpRequestMessage request)
+        {
+            var token = await Authenticator.GetToken();
+            request.Headers.Add("Authorization", $"Bearer ${token}");
         }
     }
 }
