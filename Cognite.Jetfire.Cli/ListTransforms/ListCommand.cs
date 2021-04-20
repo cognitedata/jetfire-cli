23f66a633c4bb4fbc9f74d77ef7d455fe830f67d
Mathias Lohne <mathias.lohne@cognite.com>
2021-04-20 09:32:23 +0200

    Minor improvements to list (#22)
     * Sort list output
     * Add last run status to list
    
    Co-authored-by: Stanislav Lukeš <exyi@outlook.cz>

--- Cognite.Jetfire.Cli/ListTransforms/ListCommand.cs
+++ Cognite.Jetfire.Cli/ListTransforms/ListCommand.cs
@@ -29,34 +29,41 @@ namespace Cognite.Jetfire.Cli.ListTransforms
             {
                 var list = await client.TransformConfigList(new System.Threading.CancellationToken());
 
+                // Sort on ID descending (and thus created at time)
+                list.Sort((x, y) => x.Id.CompareTo(y.Id));
+
                 // Width of columns (initialized with length of col title)
                 var IdLength = 2;
                 var ExternalIdLength = 11;
                 var NameLength = 4;
                 var ScheduleLength = 8;
+                var LastRunLength = 7;
 
                 // Calculate col width
-                List<Tuple<string, string, string, string>> rows = new List<Tuple<string, string, string, string>>();
+                var rows = new List<(string id, string extid, string name, string sched, string lastRun)>();
                 foreach (var transformConfig in list)
                 {
                     var id = transformConfig.Id.ToString();
                     var extid = transformConfig.ExternalId ?? "";
                     var name = transformConfig.Name;
                     var sched = transformConfig?.Schedule?.ToString() ?? "";
+                    var fullLastRun = await client.TransformConfigRecentJobs(transformConfig.Id, 1);
+                    var lastRun = fullLastRun.Length > 0 ? fullLastRun[0].Status() : "";
 
                     IdLength = Math.Max(IdLength, id.Length);
                     ExternalIdLength = Math.Max(ExternalIdLength, extid.Length);
                     NameLength = Math.Max(NameLength, name.Length);
                     ScheduleLength = Math.Max(ScheduleLength, sched.Length);
+                    LastRunLength = Math.Max(LastRunLength, lastRun.Length);
 
-                    rows.Add(Tuple.Create(id, extid, name, sched));
+                    rows.Add((id, extid, name, sched, lastRun));
                 }
 
                 // Print table
-                Console.WriteLine($"{"ID".PadRight(IdLength)}  {"EXTERNAL ID".PadRight(ExternalIdLength)}  {"NAME".PadRight(NameLength)}  {"SCHEDULE".PadRight(ScheduleLength)}");
+                Console.WriteLine($"{"ID".PadRight(IdLength)}  {"EXTERNAL ID".PadRight(ExternalIdLength)}  {"NAME".PadRight(NameLength)}  {"SCHEDULE".PadRight(ScheduleLength)}  {"LAST RUN".PadRight(LastRunLength)}");
                 foreach (var row in rows)
                 {
-                    Console.WriteLine($"{row.Item1.PadRight(IdLength)}  {row.Item2.PadRight(ExternalIdLength)}  {row.Item3.PadRight(NameLength)}  {row.Item4.PadRight(ScheduleLength)}");
+                    Console.WriteLine($"{row.id.PadRight(IdLength)}  {row.extid.PadRight(ExternalIdLength)}  {row.name.PadRight(NameLength)}  {row.sched.PadRight(ScheduleLength)}  {row.lastRun.PadRight(LastRunLength)}");
                 }
             }
         }
